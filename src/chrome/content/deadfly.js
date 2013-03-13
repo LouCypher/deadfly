b3085f859a48dde603b557fa7004288aa00e07fb
LouCypher <zoolcar9+github@gmail.com>
2013-03-13 07:12:43 +0700

    Refactored

--- src/chrome/content/deadfly.js
+++ src/chrome/content/deadfly.js
@@ -29,75 +29,51 @@ var DeAdFly = {
   },
 
   alert: function deadfly_alert(aString) {
-    Services.prompt.alert(null, DeAdFly, aString);
+    Services.prompt.alert(null, this, aString);
   },
 
   debug: function deadfly_debug(aString) {
-    if (DeAdFly.prefs.getBoolPref("debug")) {
-      Services.console.logStringMessage(DeAdFly + ":\n" + aString);
+    if (this.prefs.getBoolPref("debug")) {
+      Services.console.logStringMessage(this + ":\n" + aString);
     }
   },
 
   expand: function deadfly_expand(aString) {
     var regx = /zzz.*(?=')/;
     if (regx.test(aString)) {
-      var path = aString.match(regx).toString().split("'")[1];
-      if (/adf.ly\/go.php/.test(path)) {
-        path = atob(path.replace(/^https?:\/\/adf.ly\/go.php\?u\=/, ""));
+      var url = aString.match(regx).toString().split("'")[1];
+      if (/adf.ly\/go.php/.test(url)) {
+        url = atob(url.replace(/^https?:\/\/adf.ly\/go.php\?u\=/, ""));
       }
-      return path;
+      return url;
     }
     return null;
   },
 
-  getFly: function deadfly_getfly(aURL, aDocument, aCallback) {
-    try {
-      DeAdFly.debug(DeAdFly.strings.getFormattedString("retrieving", [aURL]));
-      var req = new XMLHttpRequest();
-      req.open("GET", aURL, true);
-      if (Services.prefs.getIntPref("network.http.sendRefererHeader") > 0) {
-        req.setRequestHeader("Referer", aDocument.location.href);
-      }
-      req.onreadystatechange = function() {
-        if (req.readyState == 4 && req.status == 200) {
-          DeAdFly.debug(DeAdFly.strings.getFormattedString("retrieved", [aURL]));
-          aCallback(req.responseText, aURL, aDocument);
-        }
-      }
-      req.onerror = function() {
-        DeAdFly.debug(DeAdFly.strings.getFormattedString("aborting", [req.status]));
-        req.abort();
-      }
-      req.send(null);
-    } catch (ex) {
-      DeAdFly.alert(ex);
-    }
-  },
-
   action: function deadfly_action(aString, aURL, aDocument) {
-    DeAdFly.debug(DeAdFly.strings.getFormattedString("expanding", [aURL]));
-    var url = DeAdFly.expand(aString);
+    this.debug(this.strings.getFormattedString("expanding", [aURL]));
+    var url = this.expand(aString);
     if (!url) {
-      if (!DeAdFly.prefs.getBoolPref("openAdFly")) {
-        DeAdFly.alert(DeAdFly.strings.getFormattedString("not_expand", [aURL]));
+      if (!this.prefs.getBoolPref("openAdFly")) {
+        this.alert(this.strings.getFormattedString("not_expand", [aURL]));
         return;
       }
-      DeAdFly.debug(DeAdFly.strings.getFormattedString("not_expand", [aURL]));
+      this.debug(this.strings.getFormattedString("not_expand", [aURL]));
       url = aURL;
     }
 
     var referrerURI = null;
-    if (DeAdFly.prefs.getBoolPref("sendReferrer")) {
+    if (this.prefs.getBoolPref("sendReferrer")) {
       referrerURI = makeURI(aDocument.location.href);
     }
-    var getAction = DeAdFly.prefs.getIntPref("action");
+    var getAction = this.prefs.getIntPref("action");
     switch (getAction) {
       case 30: break;
       case 20:
       case 21:
       case 0: // Display the URL in Location Bar
-        if (DeAdFly.isAdFly(url)) return;
-        DeAdFly.debug(DeAdFly.strings.getFormattedString("showing", [url]));
+        if (this.isAdFly(url)) return;
+        this.debug(this.strings.getFormattedString("showing", [url]));
         gURLBar.value = url;
         gURLBar.focus();
         break;
@@ -106,13 +82,38 @@ var DeAdFly = {
       case 12:
       case 13:
       case 2: // Load in new tab
-        DeAdFly.debug(DeAdFly.strings.getFormattedString("opening", [url]));
+        this.debug(this.strings.getFormattedString("opening", [url]));
         var bgLoad = Services.prefs.getBoolPref("browser.tabs.loadInBackground");
         gBrowser.loadOneTab(url, referrerURI, null, null, bgLoad);
         break;
       default: // Load in current tab
-        DeAdFly.debug(DeAdFly.strings.getFormattedString("opening", [url]));
+        this.debug(this.strings.getFormattedString("opening", [url]));
         loadURI(url, referrerURI);
+    }
+  },
+
+  getFly: function deadfly_getfly(aURL, aDocument) {
+    var deadfly = this;
+    try {
+      this.debug(this.strings.getFormattedString("retrieving", [aURL]));
+      var req = new XMLHttpRequest();
+      req.open("GET", aURL, true);
+      if (Services.prefs.getIntPref("network.http.sendRefererHeader") > 0) {
+        req.setRequestHeader("Referer", aDocument.location.href);
+      }
+      req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+          deadfly.debug(deadfly.strings.getFormattedString("retrieved", [aURL]));
+          deadfly.action(req.responseText, aURL, aDocument);
+        }
+      }
+      req.onerror = function() {
+        deadfly.debug(deadfly.strings.getFormattedString("aborting", [req.status]));
+        req.abort();
+      }
+      req.send(null);
+    } catch (ex) {
+      this.alert(ex);
     }
   },
 
@@ -127,7 +128,7 @@ var DeAdFly = {
   },
 
   checkForUserScript: function deadfly_checkForUserScript() {
-    if (!DeAdFly.prefs.getBoolPref("redir.enabled")) return;
+    if (!this.prefs.getBoolPref("redir.enabled")) return;
 
     var types = [
       "greasemonkey-user-script", // Greasemonkey
@@ -138,39 +139,44 @@ var DeAdFly = {
     var ids = ["http://userscripts.org/users/12/adf.ly Redir@greasespot.net",
                "adf.lyRedir@httpuserscripts.orgusers12"]
 
+    var deadfly = this;
     AddonManager.getAddonsByTypes(types, function(addons) {
       var userscripts = [];
-      for (var i in addons) {
-        if (((addons[i].id === ids[0]) || (addons[i].id === ids[1]))
-            && addons[i].isActive) {
-          userscripts.push(addons[i]);
+      addons.forEach(function(addon) {
+        if (((addon.id === ids[0]) || (addon.id === ids[1]))
+            && addon.isActive) {
+          userscripts.push(addon);
         }
-      }
+      })
       if (userscripts.length) {
         var name = userscripts[0].name;
-        var message = DeAdFly.strings.getFormattedString("confirm_redir", [name, name]);
-        var disable = Services.prompt.confirmEx(null, DeAdFly, message, 1027,
+        var message = deadfly.strings.getFormattedString("confirm_redir", [name, name]);
+        var disable = Services.prompt.confirmEx(null, deadfly, message, 1027,
                                                 "", "", "", null, {value:false});
-        for (var i in userscripts) {
-          userscripts[i].userDisabled = !disable;
-        }
-        DeAdFly.prefs.setBoolPref("redir.enabled", !disable);
+        userscripts.forEach(function(userscript) {
+          userscript.userDisabled = !disable;
+        })
+        deadfly.prefs.setBoolPref("redir.enabled", !disable);
       }
     })
   },
 
   initContextMenu: function deadfly_initContextMenu(aEvent) {
-    gContextMenu.showItem("context-deadfly",
-                          gContextMenu.onLink && DeAdFly.isAdFly(gContextMenu.linkURL));
+    gContextMenu.showItem("context-deadfly", gContextMenu.onLink &&
+                                             this.isAdFly(gContextMenu.linkURL));
   },
 
   onLoad: function deadfly_onLoad(aEvent) {
     var popup = document.getElementById("contentAreaContextMenu");
-    popup.addEventListener("popupshowing", DeAdFly.initContextMenu, false);
-    popup.removeEventListener("popuphiding", DeAdFly.initContextMenu, false);
-    DeAdFly.checkForUserScript();
+    popup.addEventListener("popupshowing", deadfly_context = function(e) {
+      DeAdFly.initContextMenu(e);
+    }, false);
+    popup.removeEventListener("popuphiding", deadfly_context, false);
+    this.checkForUserScript();
   }
 }
 
-window.addEventListener("load", DeAdFly.onLoad, false);
-window.removeEventListener("unload", DeAdFly.onLoad, false);
+window.addEventListener("load", deadfly_load = function(e) {
+  DeAdFly.onLoad(e);
+}, false);
+window.removeEventListener("unload", deadfly_load, false);
